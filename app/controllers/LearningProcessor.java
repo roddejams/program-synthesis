261b86db713e8a9d6c2a2833e4a7f185e2fca9b9
James Rodden <jr1412@new-temp>
2016-04-30 17:02:49 +0100

    fix compilation error

--- app/controllers/LearningProcessor.java
+++ app/controllers/LearningProcessor.java
@@ -15,6 +15,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
