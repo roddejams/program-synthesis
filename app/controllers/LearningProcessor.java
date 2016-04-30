e8e3b091d080070a7167746e2cf876afda275fea
James <jr1412@ic.ac.uk>
2016-04-30 17:49:28 +0100

    Fix import

--- app/controllers/LearningProcessor.java
+++ app/controllers/LearningProcessor.java
@@ -16,7 +16,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
