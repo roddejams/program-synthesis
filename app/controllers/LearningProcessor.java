326ce9d7eced4b276afb72b4ce0fbfa01d204849
James <jr1412@ic.ac.uk>
2016-05-04 11:41:10 +0100

    Switch to using processbuilder

--- app/controllers/LearningProcessor.java
+++ app/controllers/LearningProcessor.java
@@ -333,7 +333,10 @@ public class LearningProcessor extends UntypedActor {
             argString = argString.replace("]", "");
             argString = argString.replace(",", " ");
 
-            Process proc = rt.exec(String.format("%s %s", haskellExe, argString));
+            ProcessBuilder pb = new ProcessBuilder(haskellExe, argString);
+            //Process proc = rt.exec(String.format("%s %s", haskellExe, argString));
+            Process proc = pb.start();
+
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 
             String s;
